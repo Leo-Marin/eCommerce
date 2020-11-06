a78e5b29742398ddde37211c97ceb816104cb3e0
Leo-Marin <leo.marin@etu.umontpellier.fr>
2020-11-06 16:41:12 +0100

    le retour de leo

---
+++ leo/controller/routeur.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once File::build_path(array("controller","ControllerVoiture.php"));
+
+// On recupère l'action passée dans l'URL
+$action = $_GET['action'];
+// Appel de la méthode statique $action de ControllerVoiture
+ControllerVoiture::$action(); 
+?>
