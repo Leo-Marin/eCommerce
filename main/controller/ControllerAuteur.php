7f145179d2f8842e30defad6996867cfa315bd7a
Paulin-Zumstein <paulin.zumstein@etu.umontpellier.fr>
2020-11-08 00:36:19 +0100

    Auteur a finir

--- main/controller/ControllerAuteur.php
+++ main/controller/ControllerAuteur.php
@@ -1,16 +1,51 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once File::build_path(["model", "ModelAuteur.php"]);
 
-/**
- * Description of ControllerAuteur
- *
- * @author leoledino
- */
 class ControllerAuteur {
-    //put your code here
+
+    public static function readAll() {
+        $view = 'listAuteur';
+        $pagetitle = 'Liste des auteurs';
+        $controller = 'voiture';
+        $tab_v = ModelAuteur::getAllAuteurs();     //appel au modèle pour gerer la BD
+        require File::build_path(array("view", "view.php"));  //"redirige" vers la vue
+    }
+
+    public static function read() {
+        $controller = 'auteur';
+        $pagetitle = 'Auteur Caracterisations';
+        $num = $_GET['numAuteur'];
+        $n = ModelVoiture::getVoitureByImmat($num);
+
+        if ($n == null) {
+            $view = 'error';
+            require File::build_path(array("view", "view.php"));
+        } else {
+            $view = 'detailAuteur';
+            require File::build_path(array("view", "view.php"));
+        }
+    }
+
+    public static function create() {
+        $view = 'createAuteur';
+        $pagetitle = 'Creation Auteur';
+        $controller = 'voiture';
+
+        require File::build_path(array("view", "view.php"));
+    }
+
+    public static function created() {
+        $numAuteur = $_GET['numAuteur'];
+        $nom = $_GET['nom'];
+        $prenom = $_GET['prenom'];
+        $nationalite = $_GET['nationalite'];
+        $dateNaissance = $_GET['dateNaissance'];
+        $auteur1 = new ModelAuteur($numAuteur,$nom,$prenom,$nationalite,$dateNaissance);
+        $auteur1->save();
+        ControllerVoiture::readAll();
+    }
+
 }
+
+?>
