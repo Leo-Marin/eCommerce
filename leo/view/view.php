1e5bf292a215512d463ae3915bc665478ffdf698
Leo-Marin <leo.marin@etu.umontpellier.fr>
2020-11-14 22:57:56 +0100

    list avec lien

--- leo/view/view.php
+++ leo/view/view.php
@@ -13,8 +13,7 @@
     
     <body>
     <?php
-    // Si $controleur='voiture' et $view='list',
-    // alors $filepath="/chemin_du_site/view/voiture/list.php"
+
     $filepath = File::build_path(array("view", $controller, "$view.php"));
     require $filepath;
     ?>
