c4926410544b5d683ca271ba04bdc96ffd5be4e3
Paulin-Zumstein <paulin.zumstein@etu.umontpellier.fr>
2020-12-06 01:13:16 +0100

    bonne avancée

--- paulin/view/view.php
+++ paulin/view/view.php
@@ -6,16 +6,16 @@
     <ul>
         <?php
         if (Session::is_admin()) {
-            echo '<li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll&controller=utilisateur>Les utilisateur</a></li>';
+            echo '<li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll&controller=utilisateur>Les Utilisateur</a></li>';
         } 
         ?>
         <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll&controller=livre>Les Livres</a></li>
         <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll&controller=auteur> Les Auteurs</a></li>
-        <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll&controller=editeur> editeurs</a></li>
-        <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll&controller=commande>Mon panier</a></li>
+        <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll&controller=editeur> Les Editeurs</a></li>
         <?php
         if (!empty($_SESSION['login'])) {
-            echo '<li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readLaSessions&controller=utilisateur> Mon compte</a></li>
+            echo '<li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll&controller=commande>Mes Commandes</a></li>';
+            echo '<li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readLaSessions&controller=utilisateur> Mon Compte</a></li>
         <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href="index.php?action=deconnect&controller=utilisateur">Deconnexion</a></li>';
         } else {
             echo '<li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href="index.php?action=connect&controller=utilisateur">Connexion</a></li>
